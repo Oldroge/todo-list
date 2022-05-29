28b10a61c6cd92e4f065bd45e6185ce1036d8025
OldRoge <rogeriomrjr@icloud.com>
2022-05-28 21:27:10 -0300

    build: set the provider to the index.jsx file

--- client-side/src/index.jsx
+++ client-side/src/index.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import Store from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={Store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
