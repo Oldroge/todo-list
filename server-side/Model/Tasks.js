529d3a6b7ad0856cd98d889fc4f9a71867ced0b9
OldRoge <rogeriomrjr@icloud.com>
2022-06-03 20:25:57 -0300

    fix: fixed async variable declared without a reserved word await

--- server-side/Model/Tasks.js
+++ server-side/Model/Tasks.js
@@ -2,10 +2,12 @@
 const connection = require('./Connections');
 
 const getAllTasks = async () => {
-  const [tasks_datas] = connection.execute(
-    'SELECT * FROM tasks_datas',
+  const [tasks_datas] = await connection.execute(
+    'SELECT * FROM todo_list.tasks_datas',
   );
   return tasks_datas;
 };
 
-module.exports = { getAllTasks };
+module.exports = {
+  getAllTasks,
+};
