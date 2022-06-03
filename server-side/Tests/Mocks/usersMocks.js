86910e48afc945b4750f799968fed25a3afd248a
OldRoge <rogeriomrjr@icloud.com>
2022-06-03 17:04:42 -0300

    test: change a mock user to a file usersMocks.js and clean userTests.test file

---
+++ server-side/Tests/Mocks/usersMocks.js
@@ -0,0 +1,18 @@
+const mockUserDataBase = [
+  {
+    id: 1,
+    full_name: 'Marina Conrad Polli da Silva',
+    email: 'marinacpolli@hotmail.com',
+    password: 'alaska104',
+    token: 'LGYjwAVYeN1doVRezidaIQvcblj629801ff5a5d4',
+  },
+  {
+    id: 2,
+    full_name: 'Rogério Martins Rocha Junior',
+    email: 'rogeriomrjr@gmail.com',
+    password: 'alaska104',
+    token: 'oMmGVwMcc1zge9JJOyvSA4kAi6p629802354dc5c',
+  },
+];
+
+module.exports = mockUserDataBase;
