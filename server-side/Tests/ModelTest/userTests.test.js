0f7416b30dcff66a818c0ec07f3c098ae4600d8f
OldRoge <rogeriomrjr@icloud.com>
2022-06-06 19:28:07 -0300

    test: create a new test that tests a function create a new user

--- server-side/Tests/ModelTest/userTests.test.js
+++ server-side/Tests/ModelTest/userTests.test.js
@@ -1,10 +1,22 @@
+/* eslint-disable camelcase */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-undef */
 const { expect } = require('chai');
 
-const { getUsers } = require('../../Model/Users');
+const { getUsers, addNewUser } = require('../../Model/Users');
 
-describe('Test users model', () => {
+const { mockNewUser } = require('../Mocks/usersMocks');
+
+const {
+  DELETE_USER_DATAS,
+  DELETE_TASKS_DATAS,
+  POST_USER_DATAS,
+  POST_TASKS_DATAS,
+  SET_FOREIGN_KEY,
+} = require('../dbs_scripts/dbs');
+const db = require('../../Model/Connections');
+
+describe.only('Test users model', () => {
   it('Should return an object array', async () => {
     const getAllUsers = await getUsers();
 
@@ -15,5 +27,24 @@ describe('Test users model', () => {
     const getAllUsers = await getUsers();
 
     expect(getAllUsers).to.have.lengthOf.at.least(1);
+  });
+
+  it('Should insert a new user and return especific datas', async () => {
+    await db.execute(DELETE_USER_DATAS);
+    await db.execute(DELETE_TASKS_DATAS);
+
+    const { id, full_name, email } = mockNewUser;
+
+    const addUser = await addNewUser(mockNewUser);
+    const getAllUsers = await getUsers();
+    expect(getAllUsers).to.have.lengthOf(1);
+
+    expect(addUser).to.have.deep.keys({
+      id, full_name, email,
+    });
+
+    await db.execute(SET_FOREIGN_KEY);
+    await db.execute(POST_USER_DATAS);
+    await db.execute(POST_TASKS_DATAS);
   });
 });
