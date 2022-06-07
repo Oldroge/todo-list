f6f5354415eb4907991a91210605cd53ad0da1d0
OldRoge <rogeriomrjr@icloud.com>
2022-06-06 21:12:58 -0300

    test: create a test to the service stack when input the valid datas

--- server-side/Tests/ServiceTest/userService.test.js
+++ server-side/Tests/ServiceTest/userService.test.js
@@ -6,10 +6,16 @@ const { addNewUser } = require('../../Service/usersService');
 const { mockNewUser } = require('../Mocks/usersMocks');
 
 describe.only('Tests user Service', () => {
-  it('When informed data isnt valid should return false', async () => {
+  it('When input invalid datas, should return false', async () => {
     const addUser = await addNewUser(mockNewUser);
 
     expect(addUser).to.be.a('boolean');
     expect(addUser).to.be.equal(false);
+  });
+  it('When input valid datas, should return true', async () => {
+    const addUser = await addNewUser(mockNewUser);
+
+    expect(addUser).to.be.a('boolean');
+    expect(addUser).to.be.equal(true);
   });
 });
