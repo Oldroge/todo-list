3af872680635753d046a0546536f9a78639c6913
OldRoge <rogeriomrjr@icloud.com>
2022-06-06 21:22:59 -0300

    test: create that only accept query with especific keys

--- server-side/Tests/ServiceTest/userService.test.js
+++ server-side/Tests/ServiceTest/userService.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-undef */
 const { expect } = require('chai');
 
@@ -17,5 +18,16 @@ describe.only('Tests user Service', () => {
 
     expect(addUser).to.be.a('boolean');
     expect(addUser).to.be.equal(true);
+  });
+  it('Object should have id, full_name, email, password and token keys', async () => {
+    const {
+      id, full_name, email, password, token,
+    } = mockNewUser;
+
+    const addUser = await addNewUser(mockNewUser);
+
+    expect(addUser).to.have.deep.keys({
+      id, full_name, email, password, token,
+    });
   });
 });
