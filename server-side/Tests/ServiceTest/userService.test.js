748ca1fad790d8dc1df1ee566156ea75d081219c
OldRoge <rogeriomrjr@icloud.com>
2022-06-07 11:50:36 -0300

    test: add a new describe to do test more informative

--- server-side/Tests/ServiceTest/userService.test.js
+++ server-side/Tests/ServiceTest/userService.test.js
@@ -6,40 +6,42 @@ const { addNewUser } = require('../../Service/usersService');
 
 const { mockNewUser } = require('../Mocks/usersMocks');
 
-describe.only('Tests user Service', () => {
-  it('When input invalid datas, should return false', async () => {
-    const addUser = await addNewUser(mockNewUser);
+describe('Tests user Service', () => {
+  describe('Test the function where add a new user', () => {
+    it('When input invalid datas, should return false', async () => {
+      const addUser = await addNewUser(mockNewUser);
 
-    expect(addUser).to.be.a('boolean');
-    expect(addUser).to.be.equal(false);
-  });
-  it('When input valid datas, should return true', async () => {
-    const addUser = await addNewUser(mockNewUser);
-
-    expect(addUser).to.be.a('boolean');
-    expect(addUser).to.be.equal(true);
-  });
-  it('Object should have id, full_name, email, password and token keys', async () => {
-    const {
-      id, full_name, email, password, token,
-    } = mockNewUser;
-
-    const addUser = await addNewUser(mockNewUser);
-
-    expect(addUser).to.have.deep.keys({
-      id, full_name, email, password, token,
+      expect(addUser).to.be.a('boolean');
+      expect(addUser).to.be.equal(false);
     });
-  });
+    it('When input valid datas, should return true', async () => {
+      const addUser = await addNewUser(mockNewUser);
 
-  it('The keys from object should return the right types', async () => {
-    const {
-      id, full_name, email, password, token,
-    } = await addNewUser(mockNewUser);
+      expect(addUser).to.be.a('boolean');
+      expect(addUser).to.be.equal(true);
+    });
+    it('Object should have id, full_name, email, password and token keys', async () => {
+      const {
+        id, full_name, email, password, token,
+      } = mockNewUser;
 
-    expect(id).to.be.a('number');
-    expect(full_name).to.be.a('string');
-    expect(email).to.be.a('string');
-    expect(password).to.be.a('string');
-    expect(token).to.be.a('string');
-  });
+      const addUser = await addNewUser(mockNewUser);
+
+      expect(addUser).to.have.deep.keys({
+        id, full_name, email, password, token,
+      });
+    });
+
+    it('The keys from object should return the right types', async () => {
+      const {
+        id, full_name, email, password, token,
+      } = await addNewUser(mockNewUser);
+
+      expect(id).to.be.a('number');
+      expect(full_name).to.be.a('string');
+      expect(email).to.be.a('string');
+      expect(password).to.be.a('string');
+      expect(token).to.be.a('string');
+    });
+  })
 });
