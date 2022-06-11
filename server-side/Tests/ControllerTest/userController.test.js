18bdbd05b5fb5e9ebc9845cf3a99098b836ce567
OldRoge <rogeriomrjr@icloud.com>
2022-06-10 21:59:17 -0300

    test: check if password] field exists

--- server-side/Tests/ControllerTest/userController.test.js
+++ server-side/Tests/ControllerTest/userController.test.js
@@ -53,5 +53,20 @@ describe('Test user controller', () => {
           expect(result.message).toBe('Invalid entries. Try again');
         });
     });
+
+    it('Field password should is required', async () => {
+      await frisby.post(URL, addUser({
+        id: 3,
+        full_name: 'Alaska da Silva Polli',
+        email: 'alaska@gmail.com',
+        token: 'oMmRZmWcc1zge9JJOyvSA4kAi6p629802354dc5c',
+      }))
+        .expect('status', 400)
+        .then((response) => {
+          const { body } = response;
+          const result = JSON.parse(body);
+          expect(result.message).toBe('Invalid entries. Try again');
+        });
+    });
   });
 });
